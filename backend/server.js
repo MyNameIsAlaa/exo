34370a1b31a678c8bdd0ae51c859e32f6622423d
Alaa <alaa0selim@gmail.com>
2021-12-20 17:13:12 +0200

    enable cros and install body-parser

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// enable CORS
+app.use(cors()); 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
 mongoose.connect(config.DB_URL,(error)=>{
     error? console.log('mongoose error:', error): null;
 })
 
 const moviesRoutes = require("./routes/movies");
 const favoritesRoutes = require("./routes/favorites");
-
 
 
 app.use('/movies', moviesRoutes); // Movies Endpoint
